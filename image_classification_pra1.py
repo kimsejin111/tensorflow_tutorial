8f8ac74704c36f56bd959a031527fe1d18f3fa70
kimsejin111 <kimtpwjd@ajou.ac.kr>
2021-12-06 00:17:22 +0900

    1206_1

--- image_classification_pra1.py
+++ image_classification_pra1.py
@@ -130,3 +130,26 @@ history=model.fit(
 )
 
 '''Training result visualization'''
+acc=history.history['accuracy']
+val_acc=history.history['val_accuracy']
+
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+
+epochs_range= range(epochs)
+
+plt.figure(figsize=(8,8))
+plt.subplot(1,2,1)
+plt.plot(epochs_range,acc,label="Training ACC")
+plt.plot(epochs_range,val_acc,label="Validation ACC")
+plt.legend(loc='lower right')
+plt.title('Training and Validation Accuracy')
+
+plt.subplot(1,2,2)
+plt.plot(epochs_range,loss,label="Training Loss")
+plt.plot(epochs_range,val_loss,label="Validation Loss")
+plt.legend(loc='upper right')
+plt.title('Training and Validation Loss')
+
+plt.show()
+
